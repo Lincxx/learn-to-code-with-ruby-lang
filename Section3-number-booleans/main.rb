ce0fb044d8a63a9efc27fc9d3a6fa355b6b68872
Lincxx <bossco28@gmail.com>
2021-12-22 08:02:46 -0500

    Add Section 3

---
+++ Section3-number-booleans/main.rb
@@ -0,0 +1,43 @@
+#------------numbers and .class method-------
+p 5.next
+p -2.class
+p 2.34.class
+p -3.4.next_float
+p 99999999999999999999999999999999999999999999999999999999999999999999999999999.next
+
+#------------convert numbers to strings and vice versa-------
+str = "5"
+p str.class
+p str.to_i.class
+p str.to_f.class
+
+# this is still a str
+p str
+
+age = 25
+p age.class
+p age.to_s.class
+
+#------------intro booleans-------
+
+#------------the .odd? and .even? predicate methods-------
+
+#------------comparsion with the equality (==) operator-------
+
+#------------comparsion with the inequality (!=) operator-------
+
+#------------less than and greater than operators-------
+
+#------------arithmetic methods and basics arguments-------
+
+#------------intro to object methods with parameter (the .between?)-------
+
+#------------float methods-------
+
+#------------assignment operators-------
+
+#------------intro to blocks with the .times method-------
+
+#------------.upto and .downto methods-------
+
+#------------.step method-------
---
+++ Section3-number-booleans/readme.md
@@ -0,0 +1,16 @@
+# Topics Covered
+
+- numbers and .class method
+- convert numbers to strings and vice versa
+- intro booleans
+- the .odd? and .even? predicate methods
+- comparsion with the equality (==) operator
+- comparsion with the inequality (!=) operator
+- less than and greater than operators
+- arithmetic methods and basics arguments
+- intro to object methods with parameter (the .between?)
+- float methods
+- assignment operators
+- intro to blocks with the .times method
+- .upto and .downto methods
+- .step method
