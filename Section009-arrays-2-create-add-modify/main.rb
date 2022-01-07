8bf2c0f04f7c81a7144cff2e525b0531ff46415d
Lincxx <bossco28@gmail.com>
2022-01-07 06:01:03 -0500

    Added in the topics

---
+++ Section009-arrays-2-create-add-modify/main.rb
@@ -0,0 +1,22 @@
+#----------- the ri program
+#----------- review blocks
+#----------- the .each method on an array
+#----------- more practice with the .each method
+#----------- each within each
+#----------- the for loop
+#----------- the .each_with_index method
+#----------- challenge the .each_with_index
+#----------- the .map and .collect methods on an array
+#----------- iteration over array with while or until loops
+#----------- the break keyword
+#----------- the next keyword
+#----------- the .reverse method on an array
+#----------- the .sort method on an array
+#----------- the .concat method on an array
+#----------- the .max and .min method on an array
+#----------- the .include? predicate method on an array
+#----------- the .index and .find_index method on an array
+#----------- the .select method on an array
+#----------- the .reject method on an array
+#----------- the unpack a mmultidimensional array
+#----------- coding exercise 2 - even or odd
