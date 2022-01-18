c4559eaf78606b01330744a7d915d41ef2d6a479
Lincxx <bossco28@gmail.com>
2022-01-18 12:48:27 -0500

    Finished Section 18

--- Section018-classes-1/main.rb
+++ Section018-classes-1/main.rb
@@ -18,18 +18,18 @@
 # A class ic called an abstract type because it hides the complexity of interaction with the data, much like a method abstracts the complexity of a process
 
 #----------- review of the .class method on ruby objects
-puts 5.class
-puts 8.class
-puts 8.class == 5.class
+# puts 5.class
+# puts 8.class
+# puts 8.class == 5.class
 
-puts 5.4.class
-puts 3.43.class
+# puts 5.4.class
+# puts 3.43.class
 
-puts 3.43.class == true.class
+# puts 3.43.class == true.class
 
-puts "Hello".class  
-puts [1,2,3].class  
-puts lambda {}.class
+# puts "Hello".class  
+# puts [1,2,3].class  
+# puts lambda {}.class
 
 #----------- the .superclass and .ancestors methods on a class
 ## The .class method and objects
@@ -47,27 +47,27 @@ puts lambda {}.class
 # a superclass is the class that a current class inherits from
 # the .ancestor method can also be called on a class. It returns an array of all superclasses that the class inherits from
 
-p 5.class
-p 5.class.superclass
-p 5.class.superclass.superclass
-p 5.class.superclass.superclass.superclass
+# p 5.class
+# p 5.class.superclass
+# p 5.class.superclass.superclass
+# p 5.class.superclass.superclass.superclass
 
-p 5.class.ancestors # comparable and Kernel are modules
+# p 5.class.ancestors # comparable and Kernel are modules
 
-#----------- the .methods method
-int_methods =  5.methods.sort
-float_methods = 2.3.methods.sort
+# #----------- the .methods method
+# int_methods =  5.methods.sort
+# float_methods = 2.3.methods.sort
 
 
-# & is union, recall from the arrays section
-puts int_methods & float_methods
-# - is minus (differnce in the 1st), recall from the arrays section
-puts int_methods - float_methods
+# # & is union, recall from the arrays section
+# puts int_methods & float_methods
+# # - is minus (differnce in the 1st), recall from the arrays section
+# puts int_methods - float_methods
 
-array_methods = [1,2,3].methods.sort
-hash_methods = {key: "value"}.methods.sort
+# array_methods = [1,2,3].methods.sort
+# hash_methods = {key: "value"}.methods.sort
 
-puts array_methods - hash_methods
+# puts array_methods - hash_methods
 
 
 #----------- create a class
@@ -96,31 +96,237 @@ puts array_methods - hash_methods
 # If the initialize method is not defined in the class, each object is initialized with no state (like that Gadget objects in the previous lesson)
 # The assignment of values to instance variables does not mean the values have to stay constant. The object's state can be altered later.
 
+# class Gadget
+
+#   def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "topsecret"
+#     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+#   end
+
+# end
+
+# phone = Gadget.new
+# puts phone.inspect
+# p phone.instance_variables
+
+# laptop = Gadget.new
+# puts laptop.inspect
+# p laptop
+
+
+
 #----------- coding exercise 6 create classes with instance variables
 
 
 #----------- instance methods
+# class Gadget
 
+#   def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "topsecret"
+#     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+#   end
+
+#   # instance method - we are referring to the instance variables
+#   def info
+#     "Gadget #{@production_number} has the username #{@username}"
+#   end
+# end
+
+# phone = Gadget.new
+# puts phone.inspect
+# p phone.instance_variables
+# p phone.info
+
+
+# # p phone.methods.sort
+# #here we can get the difference/s of methods
+# puts phone.methods - Object.methods
 
 #----------- coding exercise 7 instance methods
 
 
 #----------- override the .to_s method
+# class Gadget
+
+#   def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "topsecret"
+#     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+#   end
+
+
+#   def to_s 
+#     "Gadget #{@production_number} has the username #{@username}"
+#   end
+# end
+
+# phone = Gadget.new
+# puts phone.inspect
+# p phone.instance_variables
+
+# p phone.methods.sort
+# p phone.respond_to?(:to_s)
+# p phone.to_s
+
 
 
 #----------- coding exercise 8 the .to_s method
 
 
 #----------- the self keyword in an instance method
+# class Gadget
+
+#   def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "topsecret"
+#     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+#   end
+
+
+#   def to_s 
+#     "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
+#   end
+# end
+
+# phone = Gadget.new
+# p phone.to_s
 
 
 #----------- getter methods
+# class Gadget
 
+#   def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "topsecret"
+#     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+#   end
+
+
+#   def to_s 
+#     "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
+#   end
+
+#   # eg, of getter methods
+#   def username
+#     @username
+#   end
+
+#   def production_number
+#     @production_number
+#   end
+
+# end
+
+# phone = Gadget.new
+# p phone.username
+# p phone.production_number
+# puts phone.inspect
 
 #----------- setter methods
+# class Gadget
 
+#   def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "topsecret"
+#     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+#   end
+
+
+#   def to_s 
+#     "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
+#   end
+
+#   # eg, of getter methods
+#   def username
+#     @username
+#   end
+
+#   def production_number
+#     @production_number
+#   end
+
+#   # eg, of setter methods, also called writer methods
+#   def username=(new_username)
+#     @username = new_username
+#   end
+  
+#   def password=(new_password)
+#     @password = new_password
+#   end
+
+# end
+
+# phone = Gadget.new
+# p phone.username
+# p phone.production_number
+# # p phone.password
+# p phone.password = "qwerty"
+# puts phone.inspect
 
 #----------- shortcut accessor methods
 
+# class Gadget
+#   # aka getter and setter
+#   attr_accessor :username
+#   # aka getter
+#   attr_reader :production_number
+#   # aka setter
+#   attr_writer :password
+  
+
+
+#   def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "topsecret"
+#     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+#   end
+
+
+#   def to_s 
+#     "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
+#   end
+
+ 
+# end
+
+# phone = Gadget.new
+# p phone.username
+# p phone.production_number
+# # p phone.password
+# p phone.password = "asdfgh"
+# puts phone.inspect
 
 #----------- add parameters to initialize method
+
+class Gadget
+  # aka getter and setter
+  attr_accessor :username
+  # aka getter
+  attr_reader :production_number
+  # aka setter
+  attr_writer :password
+  
+
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+
+
+  def to_s 
+    "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
+  end
+
+ 
+end
+
+g1 = Gadget.new("jeremy", "qwerty")
+
+p g1.to_s
+
+
+
